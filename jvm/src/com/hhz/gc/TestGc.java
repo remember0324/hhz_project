336b4d1a160508460c6d1235f10a204c7a3324c2
remember0324 <1367225405@qq.com>
2019-08-24 22:17:18 +0800

    jvm优化-2019.8.24

---
+++ jvm/src/com/hhz/gc/TestGc.java
@@ -0,0 +1,37 @@
+package com.hhz.gc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.Random;
+
+/**
+ * @Author hehongzhi
+ * @Date 2019-08-24
+ * @Version 1.0
+ */
+
+public class TestGc {
+
+    public static void main(String[] args) throws Exception {
+
+        //实现:不断的产生新的数据(对象),生育户的废弃对象(垃圾)
+        List<Object> list = new ArrayList<>();
+
+        while (true) {
+            int sleep = new Random().nextInt(100);
+            if (System.currentTimeMillis() % 2 == 0) {
+                list.clear();
+            } else {
+                //向list添加10000个对象并且设置最大内存为16M
+                for (int i = 0; i < 10000; i++) {
+                    Properties properties = new Properties();
+                    properties.put("key_" + i, "value_" + System.currentTimeMillis());
+                    list.add(properties);
+                }
+            }
+            Thread.sleep(sleep);
+        }
+
+    }
+}
